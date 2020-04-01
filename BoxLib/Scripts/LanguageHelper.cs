ef35fe121ca8a341ca1cedf579c70190748dfef1
LunarDiver <25Prozent@gmail.com>
2020-04-01 14:08:05 +0200

    Added language helper class.

---
+++ BoxLib/Scripts/LanguageHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace BoxLib.Scripts
+{
+	public class LanguageHelper
+	{
+		public static Type Resources;
+
+		/// <summary>
+		/// Changes the currently active language of this project.
+		/// </summary>
+		/// <param name="culture">The culture to change to.</param>
+		public static void ChangeLanguage(string culture, Type resources = null)
+		{
+			ChangeLanguage(new CultureInfo(culture), resources);
+		}
+
+		public static void ChangeLanguage(CultureInfo culture, Type resources = null)
+		{
+			if(resources == null)
+				resources = Resources;
+
+			resources?.GetProperty("Culture")?.SetValue(null, culture);
+		}
+	}
+}
