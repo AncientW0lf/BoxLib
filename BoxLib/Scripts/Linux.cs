e0eecc1de8cf88a8f4840873fe6128f44612256b
LunarDiver <25Prozent@gmail.com>
2020-04-08 19:04:57 +0200

    Added new class for bash commands.

---
+++ BoxLib/Scripts/Linux.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace BoxLib.Scripts
+{
+	public static class Linux
+	{
+		public static string Bash(string command)
+		{
+			string escapedArgs = command.Replace("\"", "\\\"");
+            
+			using var process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "/bin/bash",
+					Arguments = $"-c \"{escapedArgs}\"",
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true,
+				}
+			};
+			try
+			{
+				process.Start();
+			}
+			catch(Win32Exception e)
+			{
+				return e.Message;
+			}
+			process.WaitForExit(60000);
+			string result = process.StandardOutput.ReadToEnd();
+			return result;
+		}
+	}
+}
