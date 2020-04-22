558bbc525da020db68a036760bf8baaa9591e856
LunarDiver <25Prozent@gmail.com>
2020-04-22 16:48:12 +0200

    Added missing documentation, removed static property from LanguageHelper, added new GetLanguages method to retrieve a string array from an embedded file.

--- BoxLib/Scripts/LanguageHelper.cs
+++ BoxLib/Scripts/LanguageHelper.cs
@@ -1,25 +1,61 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
+using System.Reflection;
 
 namespace BoxLib.Scripts
 {
 	public class LanguageHelper
 	{
-		public static Type Resources;
+		private readonly Type _resources;
+
+		private readonly string _langListName;
+
+		public LanguageHelper(Type resources, string langListName)
+		{
+			_resources = resources;
+			_langListName = langListName;
+		}
+
+		/// <summary>
+		/// Retrieves a list of all supported languages of a project.
+		/// </summary>
+		public string[] GetLanguages()
+		{
+			try
+			{
+				//Opens the file with a list of languages
+				using Stream languageFile = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{_resources.Name}.{_langListName}.txt");
+				using var reader = new StreamReader(languageFile ?? throw new InvalidOperationException());
+
+				//Returns the splitted string with languages
+				return reader.ReadToEnd().Split(new []{"\n", "\r"}, StringSplitOptions.RemoveEmptyEntries);
+			}
+			catch(InvalidOperationException)
+			{
+				return null;
+			}
+		}
 
 		/// <summary>
 		/// Changes the currently active language of this project.
 		/// </summary>
 		/// <param name="culture">The culture to change to.</param>
-		public static void ChangeLanguage(string culture, Type resources = null)
+		/// <param name="resources">The class that stores all resource files.</param>
+		public void ChangeLanguage(string culture, Type resources = null)
 		{
 			ChangeLanguage(new CultureInfo(culture), resources);
 		}
 
-		public static void ChangeLanguage(CultureInfo culture, Type resources = null)
+		/// <summary>
+		/// Changes the currently active language of this project.
+		/// </summary>
+		/// <param name="culture">The culture to change to.</param>
+		/// <param name="resources">The class that stores all resource files.</param>
+		public void ChangeLanguage(CultureInfo culture, Type resources = null)
 		{
 			if(resources == null)
-				resources = Resources;
+				resources = _resources;
 
 			resources?.GetProperty("Culture")?.SetValue(null, culture);
 		}
