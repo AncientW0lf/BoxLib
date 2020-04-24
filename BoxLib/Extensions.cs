9fb04f0ea3529951bebf67fcadf56db87c75da06
LunarDiver <25Prozent@gmail.com>
2020-04-24 14:32:11 +0200

    Added string extensions, added console helper class.

---
+++ BoxLib/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+namespace BoxLib
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Replaces all characters inside a <see cref="string"/> that are invalid in a file system path with a replacement character.
+		/// </summary>
+		/// <param name="input">The <see cref="string"/> to change.</param>
+		/// <param name="replacement">The replacement character used for invalid characters.</param>
+		/// <returns>The modified <see cref="string"/>.</returns>
+		public static string ReplaceInvalidPathChars(this string input, char replacement)
+		{
+			char[] invalidChars = Path.GetInvalidPathChars();
+			for(int i = 0; i < invalidChars.Length; i++)
+			{
+				input = input.Replace(invalidChars[i], replacement);
+			}
+
+			return input;
+		}
+		
+		/// <summary>
+		/// Replaces all characters inside a <see cref="string"/> that are invalid in a file system file name with a replacement character.
+		/// </summary>
+		/// <param name="input">The <see cref="string"/> to change.</param>
+		/// <param name="replacement">The replacement character used for invalid characters.</param>
+		/// <returns>The modified <see cref="string"/>.</returns>
+		public static string ReplaceInvalidFileChars(this string input, char replacement)
+		{
+			char[] invalidChars = Path.GetInvalidFileNameChars();
+			for(int i = 0; i < invalidChars.Length; i++)
+			{
+				input = input.Replace(invalidChars[i], replacement);
+			}
+
+			return input;
+		}
+	}
+}
---
+++ BoxLib/Scripts/ConsoleWinHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BoxLib.Scripts
+{
+	public static class ConsoleWinHelper
+	{
+		private const int SW_HIDE = 0;
+		private const int SW_SHOW = 5;
+
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr GetConsoleWindow();
+
+		[DllImport("user32.dll")]
+		private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+		public static void HideWindow()
+		{
+			IntPtr handle = GetConsoleWindow();
+
+			// Hide
+			ShowWindow(handle, SW_HIDE);
+		}
+
+		public static void ShowWindow()
+		{
+			IntPtr handle = GetConsoleWindow();
+
+			// Show
+			ShowWindow(handle, SW_SHOW);
+		}
+	}
+}
