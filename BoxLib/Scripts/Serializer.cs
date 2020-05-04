46b92e0e918a09688d2875c85139f912be44ff0c
LunarDiver <25Prozent@gmail.com>
2020-05-04 15:51:57 +0200

    Added XML serializer.

---
+++ BoxLib/Scripts/Serializer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace BoxLib.Scripts
+{
+	public static class Serializer
+	{
+		/// <summary>
+		/// Deserializes a string into the specified type.
+		/// </summary>
+		/// <typeparam name="T">The type to deserialize the input into.</typeparam>
+		/// <param name="input">The string to be deserialized.</param>
+		/// <returns>The deserialized object.</returns>
+		public static T Deserialize<T>(string input) where T : class
+		{
+			var ser = new XmlSerializer(typeof(T));
+
+			using (var sr = new StringReader(input))
+			{
+				return (T)ser.Deserialize(sr);
+			}
+		}
+		
+		/// <summary>
+		/// Serializes an object with the specified type into a string.
+		/// </summary>
+		/// <typeparam name="T">The type that will be serialized.</typeparam>
+		/// <param name="objectToSerialize">The object to be serialized.</param>
+		/// <returns>The serialized string.</returns>
+		public static string Serialize<T>(T objectToSerialize)
+		{
+			var xmlSerializer = new XmlSerializer(objectToSerialize.GetType());
+
+			using (var textWriter = new StringWriter())
+			{
+				xmlSerializer.Serialize(textWriter, objectToSerialize);
+				return textWriter.ToString();
+			}
+		}
+
+		/// <summary>
+		/// Perform a deep Copy of the object.
+		/// </summary>
+		/// <typeparam name="T">The type of object being copied.</typeparam>
+		/// <param name="source">The object instance to copy.</param>
+		/// <returns>The copied object.</returns>
+		public static T Clone<T>(T source) where T : class
+		{
+			if (!typeof(T).IsSerializable)
+			{
+				throw new ArgumentException("The type must be serializable.", nameof(source));
+			}
+
+			// Don't serialize a null object, simply return the default for that object
+			if (Object.ReferenceEquals(source, null))
+			{
+				return default(T);
+			}
+
+			IFormatter formatter = new BinaryFormatter();
+			Stream stream = new MemoryStream();
+			using (stream)
+			{
+				formatter.Serialize(stream, source);
+				stream.Seek(0, SeekOrigin.Begin);
+				return (T)formatter.Deserialize(stream);
+			}
+		}
+	}
+}
