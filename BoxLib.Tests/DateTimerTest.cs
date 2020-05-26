16ed535f29c144e9ef3e93bc973bad726619fd5e
LunarDiver <25Prozent@gmail.com>
2020-05-26 14:35:31 +0200

    Added unit test for DateTimer.

---
+++ BoxLib.Tests/DateTimerTest.cs
@@ -0,0 +1,52 @@
+﻿using BoxLib.Objects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BoxLib.Tests
+{
+	[TestClass]
+	public class DateTimerTest
+	{
+		[TestMethod]
+		public void TestTiming()
+		{ 
+			Task.Run(TestTimingFunc).GetAwaiter().GetResult();
+		}
+
+		private static async Task TestTimingFunc()
+		{
+			var waiter = new AutoResetEvent(false);
+
+			DateTime timing = DateTime.Now;
+			timing = timing.Subtract(new TimeSpan(0, 0, timing.Second));
+			timing = timing.Subtract(new TimeSpan(0, 0, 0, 0, timing.Millisecond));
+			timing = timing.AddMinutes(1);
+
+			using var timer = new DateTimer(10)
+			{
+				Interval = new TimeSpan(0, 1, 0),
+				AutoReset = false,
+				StartTime = timing,
+				HandleStartTimeAsElapsed = false
+			};
+			timer.Elapsed += (sender, e) =>
+			{
+				waiter.Set();
+			};
+			timer.Start();
+
+			Assert.IsTrue(timer.Enabled);
+
+			await Task.Run(() =>
+			{
+				waiter.WaitOne(60000 * 3);
+				DateTime now = DateTime.Now;
+				Assert.IsTrue(timing.AddMinutes(1).ToString("T")
+						.Equals(now.Subtract(new TimeSpan(0, 0, 0, 0, now.Millisecond)).ToString("T")), 
+					$"Timing is off! Should be {timing.AddMinutes(1):T} but is {now:T}.");
+			});
+		}
+	}
+}
