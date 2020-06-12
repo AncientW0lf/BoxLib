ba63e8a78369a62be73acb82668e6d2e8a4b1e42
LunarDiver <25Prozent@gmail.com>
2020-06-12 14:25:25 +0200

    Added test for serialization with root parameter.

---
+++ BoxLib.Tests/SerializerTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BoxLib.Tests
+{
+	[TestClass]
+	public class SerializerTest
+	{
+		[TestMethod]
+		public void TestXmlRoot()
+		{
+			string serialized = Static.Serializer.Serialize(new Containers.LineF(), "ROOT");
+			Assert.IsTrue(serialized.Contains("<ROOT") && serialized.Contains("</ROOT>"), serialized);
+		}
+	}
+}
