56aa4925ed0261d14837d3f4943b6d0fd68df3c2
LunarDiver <25Prozent@gmail.com>
2020-06-12 16:04:38 +0200

    Added encryption tests.

---
+++ BoxLib.Tests/EncodingTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BoxLib.Tests
+{
+	[TestClass]
+	public class EncodingTest
+	{
+		[TestMethod]
+		public void TestSecureKeys()
+		{
+			const string secret = "This is a secret message!";
+			byte[] key1 = Static.AESThenHMAC.NewKey();
+			byte[] key2 = Static.AESThenHMAC.NewKey();
+
+			string encrypted = Static.AESThenHMAC.SimpleEncrypt(secret, key1, key2);
+			string decrypted = Static.AESThenHMAC.SimpleDecrypt(encrypted, key1, key2);
+
+			Assert.AreEqual(secret, decrypted);
+		}
+
+		[TestMethod]
+		public void TestPassword()
+		{
+			const string secret = "This is a secret message!";
+			string password = "ThisIsMyPassword";
+
+			string encrypted = Static.AESThenHMAC.SimpleEncryptWithPassword(secret, password);
+			string decrypted = Static.AESThenHMAC.SimpleDecryptWithPassword(encrypted, password);
+
+			Assert.AreEqual(secret, decrypted);
+		}
+	}
+}
