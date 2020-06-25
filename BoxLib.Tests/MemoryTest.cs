c8b8cdfdd6994ce38b8a841ad3c3df6d1f3f4bcc
LunarDiver <25Prozent@gmail.com>
2020-06-25 16:10:43 +0200

    Added test for new memory class.

---
+++ BoxLib.Tests/MemoryTest.cs
@@ -0,0 +1,24 @@
+﻿using BoxLib.Static;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace BoxLib.Tests
+{
+	[TestClass]
+	public class MemoryTest
+	{
+		[TestMethod]
+		public void TestMemoryReduction()
+		{
+			var currProc = Process.GetCurrentProcess();
+
+			long currMemory = currProc.WorkingSet64;
+			Memory.MinimizeFootprint();
+			currProc.Refresh();
+			long newMemory = currProc.WorkingSet64;
+
+			Assert.IsTrue(newMemory < currMemory, "Memory hasn't been reduced! " +
+			                                      $"{newMemory} should be smaller than {currMemory}.");
+		}
+	}
+}
