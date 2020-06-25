eba914c1b38503f87b77a9b9b765c0e1d534fda9
LunarDiver <25Prozent@gmail.com>
2020-06-25 15:58:14 +0200

    Added new static class to handle memory.

---
+++ BoxLib/Static/Memory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BoxLib.Static
+{
+	/// <summary>
+	/// Provides methods for direct memory manipulation of your application.
+	/// </summary>
+	public static class Memory
+	{
+		[DllImport("psapi.dll")]
+		private static extern int EmptyWorkingSet(IntPtr hwProc);
+
+		/// <summary>
+		/// Reduces the working set of your application to a minimum.
+		/// This causes hard page faults if the memory is needed afterwards.
+		/// Only works on Windows.
+		/// </summary>
+		public static void MinimizeFootprint()
+		{
+			EmptyWorkingSet(Process.GetCurrentProcess().Handle);
+		}
+	}
+}
